c8571bf4ec0d94f2cff859c6c3c7a90de08facdc
malinz <m.hay@outlook.com>
2018-11-20 17:58:36 +1300

    update html get call - update name

--- layout/secure.php
+++ layout/secure.php
@@ -17,13 +17,13 @@
 /**
  * The secure layout.
  *
- * @package   theme_unitecstandard
+ * @package   theme_unitecstandard_base
  * @copyright 2013 Moodle, moodle.org
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 // Get the HTML for the settings bits.
-$html = theme_unitecstandard_get_html_for_settings($OUTPUT, $PAGE);
+$html = theme_unitecstandard_base_get_html_for_settings($OUTPUT, $PAGE);
 
 // Set default (LTR) layout mark-up for a three column page.
 $topfullwidth = 'span12';
